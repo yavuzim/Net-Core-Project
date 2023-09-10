a4190246371dd83129714c1eb4fd273718d5c16b
yavuzim <kyavuzimer@gmail.com>
2023-09-11 02:47:23 +0300

    Katmanlar olusturuldu.

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public int CategoryName { get; set; }
+        public int CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
